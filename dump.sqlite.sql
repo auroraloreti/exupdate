a7f369e759c3b2b9cf022d57fcf55daf02f66170
auroraloreti <loretiaurora16@gmail.com>
2024-08-14 18:49:51 +0200

    esercizio svolto

---
+++ dump.sqlite.sql
@@ -0,0 +1,19 @@
+-- TABLE
+CREATE TABLE books (
+  book_id INTEGER PRIMARY KEY ,
+  title TEXT NOT NULL,
+  genre TEXT NOT NULL,
+  published_year INTEGER ,
+  isbn INTEGER ,
+  price INTEGER ,
+  rating INTEGER ,
+  stock_count INTEGER 
+  , publisher TEXT, pages_number INTEGER);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
