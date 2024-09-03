b501eed4370ce22e57e88b976683ca526ab21676
auroraloreti <loretiaurora16@gmail.com>
2024-09-03 19:33:43 +0200

    esercizio modificato

--- dump.sqlite.sql
+++ dump.sqlite.sql
@@ -1,19 +1,4 @@
--- TABLE
-CREATE TABLE books (
-  book_id INTEGER PRIMARY KEY ,
-  title TEXT NOT NULL,
-  genre TEXT NOT NULL,
-  published_year INTEGER ,
-  isbn INTEGER ,
-  price INTEGER ,
-  rating INTEGER ,
-  stock_count INTEGER 
-  , publisher TEXT, pages_number INTEGER);
-CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
- 
--- INDEX
- 
--- TRIGGER
- 
--- VIEW
- 
+UPDATE Books
+SET price = 12.99,   
+    stock_count = stock_count - 1  
+WHERE title = 'War and Peace';
